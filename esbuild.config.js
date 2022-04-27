6d37643e06066cca5729d923312d7470c8040d1d
Nicholas Wilcox <nick.wilcox@cirrus.com>
2022-04-27 09:46:45 -0500

    Add clean script and make esbuild watchable

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,9 +1,38 @@
 import esbuild from "esbuild";
+import parser from "yargs-parser";
+
+function isBooleanAndTrue(value) {
+  return (typeof value === "boolean") && Boolean(value);
+}
+
+const localeTimeStringOptions = {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+};
+
+function getNowAsString() {
+  return (new Date()).toLocaleTimeString([], localeTimeStringOptions);
+}
+
+const argv = parser(process.argv.slice(2));
+const shouldWatch = isBooleanAndTrue(argv.w) || isBooleanAndTrue(argv.watch);
 
 esbuild.build({
-  entryPoints: ["src/index.ts"],
+  entryPoints: ["src/index.ts", "src/util.ts"],
   bundle: true,
   outdir: "dist",
   external: ["node_modules/*"],
   format: "esm",
+  watch: !shouldWatch
+    ? false
+    : {
+      onRebuild(error, result) {
+        const timeString = getNowAsString();
+        if (error) console.log(`${timeString}: watch build failed`);
+        else console.log(`${timeString}: watch build succeeded`);
+      },
+    },
+}).then(result => {
+  if (shouldWatch) console.log("watching...");
 });
