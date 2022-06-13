5fe157b951f0c045d6fcce09e921d71f9309acda
Nicholas Wilcox <nick.wilcox@cirrus.com>
2022-06-13 09:35:52 -0500

    lint

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ import { portal } from './portal';
 export { ModalPortal, modalController, portal };
 
 export { ModalPortalState } from './modal-portal';
-export { ModalController, KeyedTemplateResult, ModalState, ModalRegistry } from './modal-controller';
+export {
+  ModalController,
+  KeyedTemplateResult,
+  ModalState,
+  ModalRegistry,
+} from './modal-controller';
 export { PortalDirective } from './portal';
 export { StatefulElement, MapOf } from './lib/state';
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,4 +5,4 @@ import { WithLitDialog } from './with-lit-dialog';
 export { LitDialog, ConfirmModal, WithLitDialog };
 
 export { ModalSize } from './lit-dialog';
-export { GConstructor, WithLitDialogInterface } from './with-lit-dialog'; 
+export { GConstructor, WithLitDialogInterface } from './with-lit-dialog';
