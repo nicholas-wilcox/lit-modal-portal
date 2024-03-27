3e04fa608cbe633d6ade7475a4bb9183a2548d0f
Nicholas Wilcox <nwilcox@linicks.dev>
2024-03-27 10:06:20 -0500

    Format code

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -2,5 +2,5 @@ import { esbuildPlugin } from '@web/dev-server-esbuild';
 
 export default {
   nodeResolve: true,
-  plugins: [esbuildPlugin({ ts: true, target: 'es2020', tsconfig: "./tsconfig.json" })],
+  plugins: [esbuildPlugin({ ts: true, target: 'es2020', tsconfig: './tsconfig.json' })],
 };
